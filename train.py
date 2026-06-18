69b154fe38f49997caaa7be9e8a59df5f0f401f9
Sean Purcell <shpurcell@google.com>
2026-06-18 14:10:36 -0700

    v18

--- train.py
+++ train.py
@@ -91,7 +91,7 @@ class Biped(PipelineEnv):
     exclude_current_positions_from_observation=True,
     **kwargs,
   ):
-    path = ROOT_RICK_PATH / "v3Robot_v15.xml"
+    path = ROOT_RICK_PATH / "v3Robot_v18.xml"
     mj_model = mujoco.MjModel.from_xml_path(path.as_posix())
     mj_model.opt.solver = mujoco.mjtSolver.mjSOL_NEWTON
     mj_model.opt.iterations = 10 
